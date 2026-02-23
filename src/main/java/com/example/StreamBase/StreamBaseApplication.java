ba6f6f9f9d73bf4073b670b2ef0e30c0212c1703
Franco Zanier <fzanier@frba.utn.edu.ar>
2026-02-23 14:46:00 -0300

    Initial Spring Boot Project and base setup

---
+++ src/main/java/com/example/StreamBase/StreamBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.StreamBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamBaseApplication.class, args);
+	}
+
+}
